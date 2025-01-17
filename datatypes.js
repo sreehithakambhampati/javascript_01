a21d61b5fbabac9eb7ef6827f109b48b8a2a0354
sreehithakambhampati <163292233+sreehithakambhampati@users.noreply.github.com>
2025-01-17 06:41:27 +0000

    basic

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+// let score = "33";
+// console.log(typeof score)
+// let score1 = "33a"
+// console.log(typeof score1)
+
+// let score2 = Number(score)
+// console.log(score2)
+// let score3 = Number(score1)
+// console.log(score3)
+
+// Symbol---to generate unique values
+let id = Symbol(123)
+let id1 = Symbol(123)
+console.log(id===id1)
