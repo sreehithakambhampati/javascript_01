aead35af2202b5e7a1344840dcb2104d4101350a
sreehithakambhampati <163292233+sreehithakambhampati@users.noreply.github.com>
2025-01-18 11:39:42 +0000

    basics

---
+++ basics/dates.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+//Gives the ouput as milliseconds from jan 1 1970
+let myTimeStamp = Date.now()
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
