9da737b21d21dbc7491ec49a8cbe7f100379f460
sreehithakambhampati <163292233+sreehithakambhampati@users.noreply.github.com>
2025-01-19 07:05:11 +0000

    objects and api

---
+++ basics/Objects.js
@@ -0,0 +1,40 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    //Symbol must be represented like this 
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+//once we freeze we cant make any further changes to the object
+// Object.freeze(JsUser)
+// JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+    // this is used to refer the element in the same object
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
