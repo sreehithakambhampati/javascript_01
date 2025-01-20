9fa27fbf4ec2cf600aae22d386b0d3b591e32be3
sreehithakambhampati <163292233+sreehithakambhampati@users.noreply.github.com>
2025-01-20 05:20:20 +0000

    Scope

---
+++ basics/Scopes.js
@@ -0,0 +1,15 @@
+console.log(addOne(5));
+function addOne(num)
+{
+    return num+1;
+}
+
+console.log(addTwo(5));
+const addTwo = function(num){
+      return num+2;
+}
+
+
+// Output:-
+// 6
+// Cannot access 'addTwo' before initialization
